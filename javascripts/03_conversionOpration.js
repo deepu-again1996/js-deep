e085fe11b33f98897474df085553168c83838461
deepu-again1996 <56256004+deepu-again1996@users.noreply.github.com>
2023-11-21 12:09:05 +0000

    conversion datatype

---
+++ javascripts/03_conversionOpration.js
@@ -0,0 +1,17 @@
+let score = undefined
+
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abv" => NaN
+
+let isLoggedIN = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIN);
+console.log(booleanIsLoggedIn)
