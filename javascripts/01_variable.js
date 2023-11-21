1ff4cba17a6d03d7b2b051f2aa56db4afb602db4
deepu-again1996 <56256004+deepu-again1996@users.noreply.github.com>
2023-11-21 11:24:30 +0000

    variables

---
+++ javascripts/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 15240
+let accountEmail = "deepkumar@google.com"
+var accountPassword="12542"
+accountCity="Delhi"
+let accountState;
+
+//accountId=3
+
+console.log(accountId)
+console.table([accountEmail, accountPassword, accountId, accountCity, accountState])
