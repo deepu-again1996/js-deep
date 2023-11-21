24fb6d6d4d1bd04871ef3435cd0ce08b7d35307f
deepu-again1996 <56256004+deepu-again1996@users.noreply.github.com>
2023-11-21 11:24:49 +0000

    datatype

---
+++ javascripts/02_datatpye.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+// alert(hello) // alert only for brower not for node js
+let name = "Deepak"
+let age = 27
+let isLoggedIn = false
+
+// number
+// bigint
+// string
+// boolean
+// null 
+// undefined
+// symbol
+
+
+
+// object
