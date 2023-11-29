9a6df1526b09ecc45970bfebd9161e9a55bfa437
deepu-again1996 <56256004+deepu-again1996@users.noreply.github.com>
2023-11-29 10:25:07 +0000

    num

---
+++ javascripts/07_num-maths.js
@@ -0,0 +1,12 @@
+const score=400
+const balance = new Number (500)
+console.log(balance)
+
+const num=1245
+console.log(num.toString().length)
+
+const num01=245.000
+console.log(num01.toFixed(2))
+
+const num3=1000000
+console.log(num3.toLocaleString('en-IN'))
