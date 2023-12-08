7b7861f1afc10dfb4303062e79db3c8de67898a9
deepu-again1996 <56256004+deepu-again1996@users.noreply.github.com>
2023-12-08 07:08:02 +0000

    array

--- javascripts/08_array.js
+++ javascripts/08_array.js
@@ -1,8 +1,15 @@
 const myCar=["Mruti", "hundai", "tata"]
+const myNewCar=["BNW","sport car"]
 
 
+myCar.push(myNewCar)
+console.log(myCar)
 // Array Method
 
- myCar.push("Nisaan")
- myCar.pop()
- console.log(myCar)
+//  myCar.push("Nisaan")
+//  myCar.pop()
+//  console.log(myCar)
+
+
+myCar.concat(myNewCar)
+console.log(myCar)
